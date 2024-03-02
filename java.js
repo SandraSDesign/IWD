502fdcdd4da2cc5e85524932e802b5e50186dbee
SandraSDesign <sandraschmitteldesign@gmail.com>
2024-03-02 16:19:16 -0300

    primeirocommit

---
+++ java.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevent default form submission
+    // Here you can add JavaScript code to handle form submission, like sending the data to a server via AJAX
+    alert("Formulário enviado com sucesso!"); // For demonstration purposes
+});
